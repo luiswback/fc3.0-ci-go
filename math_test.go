077219c1db73a45dad6e48e15e9d42dbea48fed2
DEV03 <luiswback@gmail.com>
2024-09-11 16:39:41 -0300

    ci: test

--- math_test.go
+++ math_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 	if total != 30 {
-		t.Errorf("Resultado errado: Resultado %d. Valor esperado: %d", total, 30)
+		t.Errorf("Resultado errado: Resultado obtido %d. Valor esperado: %d", total, 30)
 	}
 }
